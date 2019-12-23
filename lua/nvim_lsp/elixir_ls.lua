04def34f478702b7e2145bf739f8bc9bce9a26e1
Mitchell Henke <mitchell@mitchellhenke.com>
2019-12-23 10:16:01 -0600

    dunno

--- lua/nvim_lsp/elixir_ls.lua
+++ lua/nvim_lsp/elixir_ls.lua
@@ -1,17 +1,17 @@
-local nvim_lsp = require'nvim_lsp'
-local configs = require'nvim_lsp/skeleton'
+-- local nvim_lsp = require'nvim_lsp'
+-- local configs = require'nvim_lsp/skeleton'
 
-if not nvim_lsp.elixir_ls then
-  configs.elixir_ls = {
-    default_config = {
-      cmd = {'/Users/mitchellhenke/.config/elixir-ls/language_server.sh'};
-      filetypes = {'ex', 'exs', 'eex'};
-      root_dir = function(fname)
-        return nvim_lsp.util.find_git_ancestor(fname) or vim.loop.os_homedir()
-      end;
-      log_level = vim.lsp.protocol.MessageType.Warning;
-      settings = {};
-    };
-  }
-end
-nvim_lsp.elixir_ls.setup{}
+-- if not nvim_lsp.elixir_ls then
+--   configs.elixir_ls = {
+--     default_config = {
+--       cmd = {'/Users/mitchellhenke/.config/elixir-ls/language_server.sh'};
+--       filetypes = {'ex', 'exs', 'eex'};
+--       root_dir = function(fname)
+--         return nvim_lsp.util.find_git_ancestor(fname) or vim.loop.os_homedir()
+--       end;
+--       log_level = vim.lsp.protocol.MessageType.Warning;
+--       settings = {};
+--     };
+--   }
+-- end
+-- nvim_lsp.elixir_ls.setup{}
