7744e39cbe1fff7da2dc151d5fa8022fa5fb3abc
Mitchell Henke <mitchell@mitchellhenke.com>
2019-12-23 10:20:43 -0600

    dunno

--- lua/nvim_lsp/elixir_ls.lua
+++ lua/nvim_lsp/elixir_ls.lua
@@ -1,17 +1,32 @@
--- local nvim_lsp = require'nvim_lsp'
--- local configs = require'nvim_lsp/skeleton'
+local skeleton = require 'nvim_lsp/skeleton'
+local util = require 'nvim_lsp/util'
+local lsp = vim.lsp
 
--- if not nvim_lsp.elixir_ls then
---   configs.elixir_ls = {
---     default_config = {
---       cmd = {'/Users/mitchellhenke/.config/elixir-ls/language_server.sh'};
---       filetypes = {'ex', 'exs', 'eex'};
---       root_dir = function(fname)
---         return nvim_lsp.util.find_git_ancestor(fname) or vim.loop.os_homedir()
---       end;
---       log_level = vim.lsp.protocol.MessageType.Warning;
---       settings = {};
---     };
---   }
--- end
--- nvim_lsp.elixir_ls.setup{}
+skeleton.elixir_ls = {
+  default_config = util.utf8_config {
+    cmd = {'/Users/mitchellhenke/.config/elixir-ls/language_server.sh'};
+    filetypes = {"ex", "exs", "eex"};
+    root_dir = function(fname)
+      return nvim_lsp.util.find_git_ancestor(fname) or vim.loop.os_homedir()
+    end;
+    log_level = lsp.protocol.MessageType.Warning;
+    settings = {};
+  };
+  -- commands = {};
+  -- on_new_config = function(new_config) end;
+  -- on_attach = function(client, bufnr) end;
+  docs = {
+    description = [[
+https://clang.llvm.org/extra/clangd/Installation.html
+**NOTE:** Clang >= 9 is recommended! See [this issue for more](https://github.com/neovim/nvim-lsp/issues/23).
+clangd relies on a [JSON compilation database](https://clang.llvm.org/docs/JSONCompilationDatabase.html) specified
+as compile_commands.json or, for simpler projects, a compile_flags.txt.
+]];
+    default_config = {
+      root_dir = [[root_pattern("mix.exs"), root_pattern(".git")]];
+      on_init = [[function to handle changing offsetEncoding]];
+      capabilities = [[default capabilities, with offsetEncoding utf-8]];
+    };
+  };
+}
+-- vim:et ts=2 sw=2
